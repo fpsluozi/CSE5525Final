688d3030856c7fd933a1a25642cb7f3d8112110f
Martin Lo <kuanwenlo@gmail.com>
2015-04-25 17:33:07 -0400

    new

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,17 @@
 def train_preprocess():
 	lines = []
-	#f = open("test.utf8", 'r')
+	#f1 = open("test.utf8", 'r')
 	f1 = open('as_training.utf8', 'r')
 	trainingText = f1.readlines()
 	#trainingText = trainingText.encode('UTF-8')
 	segments = []
+	trainData = []
 	for line in trainingText:
 		lines.append(line.decode('utf-8'))
 		#print line.decode('utf-8')
 		segment = []
 		start = True
+		trainLine = []
 		for character in line.decode('utf-8'):
 			if(character==' ' or character=='\n'):
 				start = True
@@ -19,10 +21,11 @@ def train_preprocess():
 			if(start is False):
 				segment.append(0)
 			start = False
+			trainLine.append(character)
 		segments.append(segment)
+		trainData.append(trainLine)
 
-
-	return segments
+	return trainData, segments
 def test_preprocess():
 	lines = []
 	f2 = open('as_test.utf8', 'r')
