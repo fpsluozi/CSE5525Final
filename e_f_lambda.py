34f80a2002e54691eae27f3ea4cc961695380c64
FPSLuozi <lawrenceyiranluo@gmail.com>
2015-04-26 13:55:13 -0400

    change of indices

--- e_f_lambda.py
+++ e_f_lambda.py
@@ -1,8 +1,8 @@
-e = numpy.zeros((2,2,lenChatInt), dType=double)
-f = numpy.zeros((2,2,lenChatInt), dType=double)
-lamb = numpy.zeros((2,2,lenChatInt), dType=double)
+e = numpy.zeros((lenCharInt, 2, 2), dType=double)
+f = numpy.zeros((lenCharInt, 2, 2), dType=double)
+lamb = numpy.zeros((lenCharInt, 2, 2), dType=double)
 
 for x in range(2):
 	for y in range(2):
-		for z in range(lenChatInt):
-			lamb[x][y][z] = 1.0
+		for z in range(lenCharInt):
+			lamb[z][x][y] = 1.0
