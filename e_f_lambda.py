1dee95cd6f0704a442f433f3823828d0928efa84
FPSLuozi <lawrenceyiranluo@gmail.com>
2015-04-26 15:13:42 -0400

    introducing normalizer for e

--- e_f_lambda.py
+++ e_f_lambda.py
@@ -10,6 +10,8 @@ for x in range(2):
 		for z in range(lenCharInt):
 			lamb[z][x][y] = 1.0
 
+
+
 for sent_ind in range(len(data)):
 	sentence = data[sent_ind]
 	for x in range(1, len(sentence)):
@@ -19,10 +21,12 @@ for sent_ind in range(len(data)):
 		tag = value
 		f[char_ind][tag_prev][tag] += 1
 
-		e[char_ind][0][0] += alpha[sent_ind][x-1][0] * beta[sent_ind][x][0] * exp(lamb[char_ind][0][0])
-		e[char_ind][0][1] += alpha[sent_ind][x-1][0] * beta[sent_ind][x][1] * exp(lamb[char_ind][0][1])
-		e[char_ind][1][0] += alpha[sent_ind][x-1][1] * beta[sent_ind][x][0] * exp(lamb[char_ind][1][0])
-		e[char_ind][1][1] += alpha[sent_ind][x-1][1] * beta[sent_ind][x][1] * exp(lamb[char_ind][1][1])
+		z0 = alpha[sent_ind][len(sentence)-1][0] + alpha[sent_ind][len(sentence)-1][1]
+
+		e[char_ind][0][0] += alpha[sent_ind][x-1][0] * beta[sent_ind][x][0] * exp(lamb[char_ind][0][0]) / z0
+		e[char_ind][0][1] += alpha[sent_ind][x-1][0] * beta[sent_ind][x][1] * exp(lamb[char_ind][0][1]) / z0
+		e[char_ind][1][0] += alpha[sent_ind][x-1][1] * beta[sent_ind][x][0] * exp(lamb[char_ind][1][0]) / z0
+		e[char_ind][1][1] += alpha[sent_ind][x-1][1] * beta[sent_ind][x][1] * exp(lamb[char_ind][1][1]) / z0
 		
 
 
