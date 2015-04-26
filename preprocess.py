f48540429b655726fbc74d38922c059869456cc8
Martin Lo <kuanwenlo@gmail.com>
2015-04-25 20:18:00 -0400

    tuplize training data

--- preprocess.py
+++ preprocess.py
@@ -1,39 +1,44 @@
+cTable = {}
+cNo = 0
 def train_preprocess():
 	lines = []
 	#f1 = open("test.utf8", 'r')
 	f1 = open('as_training.utf8', 'r')
 	trainingText = f1.readlines()
 	#trainingText = trainingText.encode('UTF-8')
-	segments = []
 	trainData = []
-	cSet = set()
+	global cTable
+	global cNo
 	for line in trainingText:
 		lines.append(line.decode('utf-8'))
 		#print line.decode('utf-8')
-		segment = []
 		start = True
 		trainLine = []
+
 		for character in line.decode('utf-8'):
 			if(character==' ' or character=='\n'):
 				start = True
 				continue
 			if(start is True):
-				segment.append(1)
+				trainTuple = (character, 1)
 			if(start is False):
-				segment.append(0)
+				trainTuple = (character, 0)
 			start = False
-			trainLine.append(character)
-			cSet.add(character)
-		segments.append(segment)
+			trainLine.append(trainTuple)
+			if(character not in cTable):
+				cTable.update({character: cNo})
+				cNo = cNo + 1
+				
 		trainData.append(trainLine)
-		
-	return trainData, segments, cSet
+
+	return trainData
 def test_preprocess():
 	lines = []
 	f2 = open('as_test.utf8', 'r')
 	testText = f2.readlines()
 	testData = []
-
+	global cTable
+	global cNo
 	for line in testText:
 		lines.append(line.decode('utf-8'))
 		#print line.decode('utf-8')
@@ -44,5 +49,8 @@ def test_preprocess():
 				start = True
 				continue
 			testLine.append(character)
+			if(character not in cTable):
+				cTable.update({character: cNo})
+				cNo = cNo + 1
 		testData.append(testLine)
-	return testData
+	return testData, cTable
